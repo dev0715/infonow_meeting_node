19076ed7aea05b1c61b5d403eb55cd31a51205ac
dev0715 <toptal126@outlook.com>
2021-05-06 16:50:42 +0500

    updated create room

--- socket/events/on-create-room.ts
+++ socket/events/on-create-room.ts
@@ -1,17 +1,46 @@
 import { Socket } from "socket.io";
 import { SocketData, SocketRoom } from "../models";
 import { Redis } from "../../redis";
-import { IOEvents } from "./index";
+
+import { IOEvents, joinRoom, answerCall, startCall, createIceEventData, disconnect } from "./index";
 
 export const createRoom = (socket: Socket, rooms: SocketRoom, res: SocketData) => {
 	console.log(IOEvents.CREATE_ROOM, res.meetingId);
+
+	// VERIFY AND VALIDATE USER BEFORE JOIN //
+
+	socket.on(IOEvents.ROOM_JOIN, (res: SocketData) => {
+		joinRoom(socket, rooms, res);
+	});
+
+	socket.on(IOEvents.ANSWER_CALL, (res: SocketData) => {
+		answerCall(socket, res);
+	});
+
+	socket.on(IOEvents.START_CALL, (res: SocketData) => {
+		startCall(socket, res);
+	});
+
+	socket.on(IOEvents.CREATE_ICE_EVENT_DATA, (res: SocketData) => {
+		createIceEventData(socket, res);
+	});
+
+	socket.on(IOEvents.DISCONNECT, (res: SocketData) => {
+		disconnect(socket, rooms, res);
+	});
+
+	// TO CHECK IF ROOM EXISTS OR NOT //
 
 	if (rooms.hasOwnProperty(res.meetingId)) {
 		socket.emit(IOEvents.ROOM_EXIST);
 		return;
 	}
+
 	Redis.getInstance().set(res.meetingId, JSON.stringify(res.data));
+
 	socket.join(res.meetingId);
+
 	rooms[res.meetingId] = res.meetingId;
+
 	socket.emit(IOEvents.CALL_ON_WAIT);
 };
--- socket/index.ts
+++ socket/index.ts
@@ -18,36 +18,12 @@ export const StartSocketServer = () => {
 	// make connection with user from server side
 	io.on("connection", (socket: Socket) => {
 		console.log("New user connected");
-		// io.clients
-		//emit message from server to user
+
 		socket.emit(IOEvents.CONNECT);
 		// socket.id
 
-		// listen for message from user
 		socket.on(IOEvents.CREATE_ROOM, (res: SocketData) => {
 			createRoom(socket, rooms, res);
-		});
-
-		// listen for message from user
-		socket.on(IOEvents.ROOM_JOIN, (res: SocketData) => {
-			joinRoom(socket, rooms, res);
-		});
-
-		socket.on(IOEvents.ANSWER_CALL, (res: SocketData) => {
-			answerCall(socket, res);
-		});
-
-		socket.on(IOEvents.START_CALL, (res: SocketData) => {
-			startCall(socket, res);
-		});
-
-		socket.on(IOEvents.CREATE_ICE_EVENT_DATA, (res: SocketData) => {
-			createIceEventData(socket, res);
-		});
-
-		// when server disconnects from user
-		socket.on(IOEvents.DISCONNECT, (res: SocketData) => {
-			disconnect(socket, rooms, res);
 		});
 	});
 
