3d1bf67c3a9dc16badfe302672cc9ffa88e5f2a2
dev0715 <toptal126@outlook.com>
2021-05-25 12:33:06 +0500

    final testing with board

--- socket/events/on-connect.ts
+++ socket/events/on-connect.ts
@@ -1,27 +1,33 @@
 import { Server, Socket } from "socket.io";
 import { IOEvents } from ".";
 import { t } from "../../sequelize/locales";
+import { Logger } from "../../sequelize/utils/logger";
 import { SocketData } from "../models";
 import { OnAuthorization } from "./on-authorization";
 import { OnEndCall } from "./on-end-call";
 
-function OnLocaleSet(socket: Socket, data: SocketData = { locale:"en" }) {
-    socket.locale = data.locale ?? "en";
-    socket.t = (message: string, ...args: any) => {
-        // t.setLocale(socket.locale);
-        // return t.__(message, ...args);
-        return message
-    };
+function OnLocaleSet(socket: Socket, data: SocketData = { locale: "en" }) {
+	try {
+		socket.locale = data.locale ?? "en";
+		socket.t = (message: string, ...args: any) => {
+			// t.setLocale(socket.locale);
+			// return t.__(message, ...args);
+			return message;
+		};
+	} catch (error) {
+		Logger.error(error);
+	}
 }
 
 export function OnConnect(io: Server, socket: Socket) {
+	console.log("New User Connected");
+	OnLocaleSet(socket);
 
-    console.log("New User Connected");
-    OnLocaleSet(socket);
+	socket.on(IOEvents.AUTHORIZATION, (data) =>
+		OnAuthorization(io, socket, data)
+	);
+	socket.on(IOEvents.SET_LANGUAGE, (data) => OnLocaleSet(socket, data));
+	socket.on(IOEvents.DISCONNECT, () => OnEndCall(socket));
 
-    socket.on(IOEvents.AUTHORIZATION, data=> OnAuthorization(io, socket, data));
-    socket.on(IOEvents.SET_LANGUAGE, data=> OnLocaleSet(socket, data));
-    socket.on(IOEvents.DISCONNECT, () => OnEndCall(socket));
-
-    socket.emit(IOEvents.CONNECT);
+	socket.emit(IOEvents.CONNECT);
 }
