76964b85c7e8de55acb23549c4eae8363a8a4dd9
dev0715 <toptal126@outlook.com>
2021-09-21 18:42:39 +0500

    fix: Meeting Answer was not receiving remote User's info

--- socket/events/on-answer.ts
+++ socket/events/on-answer.ts
@@ -1,12 +1,19 @@
 import { Socket } from "socket.io";
 import { SocketData } from "../models";
 import { IOEvents } from "./index";
+import _ from 'lodash';
 
 export const OnAnswer = (socket: Socket, res: SocketData) => {
 	console.log(IOEvents.ANSWER);
 	if (socket.meetingId && res.data) {
 		socket.to(socket.meetingId).emit(IOEvents.ANSWER, {
-			data: res.data,
+            data: res.data,
+            user: _.pick(socket.user, [
+                "userId",
+                "name",
+                "roleId",
+                "profilePicture",
+            ])
 		});
 	}
 };
